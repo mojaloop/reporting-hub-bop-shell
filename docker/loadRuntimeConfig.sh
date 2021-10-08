7098ce398f9be33dd5778abda80ff7a4abba3ade
Kevin <kleyow@gmail.com>
2021-10-07 21:06:41 -0500

    chore: add remote 2 and namespace runtime env

--- docker/loadRuntimeConfig.sh
+++ docker/loadRuntimeConfig.sh
@@ -2,6 +2,9 @@
 sed -i 's#__REMOTE_1_URL__#'"$REMOTE_1_URL"'#g' /usr/share/nginx/html/runtime-env.js
 sed -i 's#__REMOTE_1_URL__#'"$REMOTE_1_URL"'#g' /usr/share/nginx/html/index.html
 
+sed -i 's#__REMOTE_2_URL__#'"$REMOTE_2_URL"'#g' /usr/share/nginx/html/runtime-env.js
+sed -i 's#__REMOTE_2_URL__#'"$REMOTE_2_URL"'#g' /usr/share/nginx/html/index.html
+
 sed -i 's#__PUBLIC_PATH__#'"$PUBLIC_PATH"'#g' /usr/share/nginx/html/index.html
 sed -i 's#__PUBLIC_PATH__#'"$PUBLIC_PATH"'#g' /usr/share/nginx/html/main.js
 
