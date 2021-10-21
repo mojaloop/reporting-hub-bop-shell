e7f81f627a7a0c42483a2b8565dafbb68bb88da0
Kevin <kleyow@gmail.com>
2021-10-21 05:47:18 -0500

    chore: remove code

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,13 +1,8 @@
 import { State } from 'store';
-import authMock from 'Auth/_mockData';
 import remotesMock from 'App/_mockData';
 import buildApis, { buildEndpointBuilder } from '@modusbox/redux-utils/lib/api';
 
 const services = {
-  kratos: {
-    baseUrl: (state: State) => state.config.api.authApiBaseUrl,
-    mock: (state: State) => state.config.api.authMockApi,
-  },
   mainApi: {
     baseUrl: (state: State) => state.config.api.remoteApiBaseUrl,
     mock: (state: State) => state.config.api.remoteMockApi,
@@ -17,11 +12,6 @@ const services = {
 const builder = buildEndpointBuilder<State>();
 
 export default buildApis({
-  whoami: builder({
-    service: services.kratos,
-    url: (state: State) => state.config.auth.whoAmIEndpoint,
-    mock: authMock,
-  }),
   // NOTE: instead of an actual api, using local json file to store
   //       remote microfrontend config
   remotes: builder({
